ee945b9796051c5899f71e7a29b0c46d02d0e03f
Jennifer Reiber Kyle <reiberkyle@gmail.com>
2016-11-24 06:17:00 -0800

    working match algorithm

---
+++ match.py
@@ -0,0 +1,28 @@
+import random
+
+# contains tuples giving pairs that should not be matched
+# (e.g. couples)
+no_match = [
+    ('mom', 'dad'),
+    ('LL', 'C'),
+    ('M', 'J')]
+
+# contains strings giving names in gift train
+people = ['mom', 'dad', 'LL', 'C', 'M', 'J']
+
+givers = people[:]
+receivers = people[:]
+matches = []
+
+for giver in givers:
+    match = False
+    while not match:
+        r_index = random.randint(0, len(receivers) - 1)
+        candidate = receivers[r_index]
+        if (giver, candidate) and (candidate, giver) \
+                not in no_match:
+            match = True
+            matches.append((giver, candidate))
+            receivers.pop(r_index)
+
+print matches
